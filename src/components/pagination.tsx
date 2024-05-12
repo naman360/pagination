7125dcdbd276c108f31410e914b0211b0ceff945
naman360 <naman.geek@gmail.com>
2024-05-12 13:21:38 +0530

    feat: added pagination component

---
+++ src/components/pagination.tsx
@@ -0,0 +1,56 @@
+type Props = {
+  pageNumber: number;
+  totalPages: number;
+  handlePageNumberChange: (newPageNumber: number) => void;
+};
+
+const Pagination = ({
+  pageNumber,
+  totalPages,
+  handlePageNumberChange,
+}: Props) => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "30px",
+      }}
+    >
+      <span
+        onClick={() => handlePageNumberChange(pageNumber - 1)}
+        style={{
+          display: pageNumber === 1 ? "none" : "block",
+          cursor: "pointer",
+        }}
+      >
+        ⬅️
+      </span>
+      {[...Array(totalPages)].map((_, i) => (
+        <span
+          onClick={() => handlePageNumberChange(i + 1)}
+          style={{
+            padding: "20px",
+            cursor: "pointer",
+            border: pageNumber === i + 1 ? "1px solid black" : "",
+          }}
+        >
+          {i + 1}
+        </span>
+      ))}
+      <span
+        onClick={() => handlePageNumberChange(pageNumber + 1)}
+        style={{
+          display: pageNumber === totalPages ? "none" : "block",
+          cursor: "pointer",
+        }}
+      >
+        ➡️
+      </span>
+    </div>
+  );
+};
+
+export default Pagination;
--- src/components/products-listing.tsx
+++ src/components/products-listing.tsx
@@ -1,28 +1,44 @@
 import { useEffect, useState } from "react";
 import ProductCard from "./product-card";
+import Pagination from "./pagination";
 
 const ProductsListing = () => {
   const [products, setProducts] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
 
   const fetchProducts = async () => {
-    const result = await fetch("https://dummyjson.com/products");
+    const result = await fetch("https://dummyjson.com/products?limit=100");
     const data = await result.json();
     if (data && data.products) setProducts(data.products);
   };
+  const handlePageNumberChange = (newPageNumber: number) => {
+    if (newPageNumber - 1 >= 0 && newPageNumber <= products.length / 10)
+      setPageNumber(newPageNumber);
+  };
+
   useEffect(() => {
     fetchProducts();
   }, []);
 
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "1fr 1fr 1fr 1fr",
-        gap: "20px",
-      }}
-    >
-      {products.length > 0 &&
-        products.map((product) => <ProductCard product={product} />)}
+    <div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr 1fr",
+          gap: "20px",
+        }}
+      >
+        {products.length > 0 &&
+          products
+            .slice(pageNumber * 10 - 10, pageNumber * 10)
+            .map((product) => <ProductCard product={product} />)}
+      </div>
+      <Pagination
+        pageNumber={pageNumber}
+        handlePageNumberChange={handlePageNumberChange}
+        totalPages={products.length / 10}
+      />
     </div>
   );
 };
