6c769270f05da49b82721cd551a90a9b91e4240a
naman360 <naman.geek@gmail.com>
2024-05-12 13:37:05 +0530

    feat: backend driven approach of pagination implememented

--- src/components/products-listing.tsx
+++ src/components/products-listing.tsx
@@ -4,21 +4,29 @@ import Pagination from "./pagination";
 
 const ProductsListing = () => {
   const [products, setProducts] = useState([]);
-  const [pageNumber, setPageNumber] = useState(1);
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(1);
 
   const fetchProducts = async () => {
-    const result = await fetch("https://dummyjson.com/products?limit=100");
+    const result = await fetch(
+      `https://dummyjson.com/products?skip=${pageNumber * 10 - 10}&limit=${10}`
+    );
     const data = await result.json();
-    if (data && data.products) setProducts(data.products);
+    if (data && data.products) {
+      setProducts(data.products);
+      setTotalPages(data.total / 10);
+    }
   };
   const handlePageNumberChange = (newPageNumber: number) => {
-    if (newPageNumber - 1 >= 0 && newPageNumber <= products.length / 10)
+    console.log(newPageNumber);
+    if (newPageNumber - 1 >= 0 && newPageNumber <= totalPages)
       setPageNumber(newPageNumber);
   };
+  console.log(pageNumber);
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [pageNumber]);
 
   return (
     <div>
@@ -29,15 +37,20 @@ const ProductsListing = () => {
           gap: "20px",
         }}
       >
-        {products.length > 0 &&
+        {/* Frontend based pagination */}
+        {/* {products.length > 0 &&
           products
             .slice(pageNumber * 10 - 10, pageNumber * 10)
-            .map((product) => <ProductCard product={product} />)}
+            .map((product) => <ProductCard product={product} />)} */}
+
+        {/* Backend driven approach */}
+        {products.length > 0 &&
+          products.map((product) => <ProductCard product={product} />)}
       </div>
       <Pagination
         pageNumber={pageNumber}
         handlePageNumberChange={handlePageNumberChange}
-        totalPages={products.length / 10}
+        totalPages={totalPages}
       />
     </div>
   );
